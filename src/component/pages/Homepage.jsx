27d7b8c78b8eba19ec89779b3e4cb058f3e6a294
maxleadadvertising-hash <maxleadadvertising@gmail.com>
2026-04-27 22:57:29 +0530

    blog14/

--- src/component/pages/Homepage.jsx
+++ src/component/pages/Homepage.jsx
@@ -1,30 +1,35 @@
-import React from "react";
-import HeroSection from "../hero"; 
-import Clientsscroll from "../clientsscroll";
-import About1 from "../pages/About1";
-import Solution1 from "../pages/Solution1";
+import React, { lazy, Suspense } from "react";
+import HeroSection from "../hero";
 import Product1 from "../../component/pages/Product1";
-import Industries1 from "../../component/pages/industries1";
-import Blog1 from "../pages/Blog1";
-import Testimonials from "../Testimonials"; 
-import Contact1 from "../../component/pages/contact1"; 
-import Clear1 from "../pages/clear1";
+
+// Lazy load below-the-fold sections
+const Clientsscroll = lazy(() => import("../clientsscroll"));
+const About1 = lazy(() => import("../pages/About1"));
+const Solution1 = lazy(() => import("../pages/Solution1"));
+const Industries1 = lazy(() => import("../../component/pages/industries1"));
+const Blog1 = lazy(() => import("../pages/Blog1"));
+const Clear1 = lazy(() => import("../pages/clear1"));
+const Testimonials = lazy(() => import("../Testimonials"));
+const Contact1 = lazy(() => import("../../component/pages/contact1"));
 
 export default function Homepage() {
   return (
     <main>
-    
-     
+      {/* Above the fold: render immediately */}
       <HeroSection />
       <Product1 />
-      <Clientsscroll />
-      <About1 />
-      <Solution1 />
-      <Industries1 />
-      <Blog1 />
-      <Clear1 />
-      <Testimonials />
-      <Contact1 />
+
+      {/* Below the fold: defer rendering */}
+      <Suspense fallback={null}>
+        <Clientsscroll />
+        <About1 />
+        <Solution1 />
+        <Industries1 />
+        <Blog1 />
+        <Clear1 />
+        <Testimonials />
+        <Contact1 />
+      </Suspense>
     </main>
   );
 }
